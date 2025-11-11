e55d6814db376a66627d0767b0ddc32301b9fb37
anhvlt-2k6 <176574466+anhvlt-2k6@users.noreply.github.com>
2025-11-11 15:15:44 +0700

    .

---
+++ .completed/Long01/src/long01/Long01.java
@@ -0,0 +1,23 @@
+package long01;
+
+import FrontEnd.UserInterface;
+
+/**
+ * Long 01 - Main entry
+ * @author CE200360 Vo Luu Tuong Anh
+ * @since 2025-09-21
+ */
+public class Long01 {
+    
+    /**
+     * Entry of the program
+     * @param args In-line command line argument
+     */
+    public static void main(String[] args) {
+        // Initialize the user interface
+        UserInterface ui = new UserInterface();
+        
+        // Show the program interface
+        ui.displayUI();
+    }
+}
